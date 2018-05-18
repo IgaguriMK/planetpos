6bd817eebc2270efe5dd38c0b00ced400221c22f
Igaguri <igagurimk@gmail.com>
2018-05-18 21:41:04 +0900

    vec, mat

--- expr/vec/vec.go
+++ expr/vec/vec.go
@@ -2,66 +2,82 @@ package vec
 
 import "math"
 
-type Vec3 struct {
+type Vec struct {
 	X float64 `json:"x"`
 	Y float64 `json:"y"`
 	Z float64 `json:"z"`
 }
 
+func (v Vec) GetX() float64 { return v.X }
+func (v Vec) GetY() float64 { return v.Y }
+func (v Vec) GetZ() float64 { return v.Z }
+
 var (
-	Zero = Vec3{0, 0, 0}
-	X    = Vec3{1, 0, 0}
-	Y    = Vec3{0, 1, 0}
-	Z    = Vec3{0, 0, 0}
-	One  = Vec3{1, 1, 1}
+	ZeroVec = Vec{0, 0, 0}
+	OneX    = Vec{1, 0, 0}
+	OneY    = Vec{0, 1, 0}
+	OneZ    = Vec{0, 0, 0}
+	One     = Vec{1, 1, 1}
 )
 
-func (v Vec3) Abs() float64 {
-	return math.Sqrt(v.X*v.X + v.Y*v.Y + v.Z*v.Z)
+type Vec3 interface {
+	GetX() float64
+	GetY() float64
+	GetZ() float64
 }
 
-func (v Vec3) Add(u Vec3) Vec3 {
-	return Vec3{
-		X: v.X + u.X,
-		Y: v.Y + u.Y,
-		Z: v.Z + u.Z,
+func Abs(v Vec3) float64 {
+	return math.Sqrt(v.GetX()*v.GetX() + v.GetY()*v.GetY() + v.GetZ()*v.GetZ())
+}
+
+func Add(v, u Vec3) Vec {
+	return Vec{
+		X: v.GetX() + u.GetX(),
+		Y: v.GetY() + u.GetY(),
+		Z: v.GetZ() + u.GetZ(),
 	}
 }
 
-func (v Vec3) Sub(u Vec3) Vec3 {
-	return Vec3{
-		X: v.X - u.X,
-		Y: v.Y - u.Y,
-		Z: v.Z - u.Z,
+func Diff(v, u Vec3) Vec {
+	return Vec{
+		X: v.GetX() - u.GetX(),
+		Y: v.GetY() - u.GetY(),
+		Z: v.GetZ() - u.GetZ(),
 	}
 }
 
-func (v Vec3) Scalar(k float64) Vec3 {
-	return Vec3{
-		X: k * v.X,
-		Y: k * v.Y,
-		Z: k * v.Z,
+func Scalar(v Vec3, k float64) Vec {
+	return Vec{
+		X: k * v.GetX(),
+		Y: k * v.GetY(),
+		Z: k * v.GetZ(),
 	}
 }
 
-func (v Vec3) Unit() Vec3 {
-	if v.X == 0 && v.Y == 0 && v.Z == 0 {
-		return Zero
+func Unit(v Vec3) Vec {
+	if v.GetX() == 0 && v.GetY() == 0 && v.GetZ() == 0 {
+		return ZeroVec
 	}
 
-	a := 1 / v.Abs()
+	a := Abs(v)
 
-	return Vec3{
-		X: a * v.X,
-		Y: a * v.Y,
-		Z: a * v.Z,
+	return Vec{
+		X: v.GetX() / a,
+		Y: v.GetY() / a,
+		Z: v.GetZ() / a,
 	}
 }
 
-func (v Vec3) Dot(u Vec3) float64 {
-	return v.X*u.X + v.Y*u.Y + v.Z*u.Z
+func Dot(v, u Vec3) float64 {
+	return v.GetX()*u.GetX() + v.GetY()*u.GetY() + v.GetZ()*u.GetZ()
 }
 
-func (v Vec3) Dist(u Vec3) float64 {
-	return u.Sub(v).Abs()
+func Dist(v, u Vec3) float64 {
+	return Abs(Diff(u, v))
+}
+
+func Near(v, u Vec3, dist float64) bool {
+	d := Diff(v, u)
+
+	return d.X*d.X+d.Y*d.Y+d.Z*d.Z < dist*dist
 }
